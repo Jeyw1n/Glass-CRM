9ca73ca9bcc7324d0eb8dd4313bd9b31c26c7836
bitweed <Oltibanta342@proton.me>
2023-11-01 08:33:08 +0300

    Таблица во вкладке "Договора" может менять размер по Y. Форма не требует поля 'debt', а высчитывает его самостоятельно.

--- glass_crm/main_forms/forms.py
+++ glass_crm/main_forms/forms.py
@@ -11,3 +11,16 @@ class AddContract(forms.ModelForm):
             'montage_date': forms.DateInput(attrs={'type': 'date'}),
             'delivery_date_by_contract': forms.DateInput(attrs={'type': 'date'}),
         }
+        exclude = ['debt']  # Исключаем поле 'debt' из формы
+
+        # !!!!!!!!!!!!!!!!!
+        # Еще нужно исключить поля 'phone', 'delivery_date' и 'montage_date'.
+
+    # Высчитываем поле 'debt'.
+    def save(self, commit=True):
+        instance = super().save(commit=False)  # Вытаскиваем экземпляр без сохранения в БД.
+        instance.debt = instance.price - instance.prepayment  # Ставим значения для 'debt'.
+
+        if commit:
+            instance.save()
+        return instance
