4ca6984a11a1fda375d74a1f02e73dfca8482b8f
bitweed <Oltibanta342@proton.me>
2023-12-23 16:16:30 +0300

    The label of the current page has been added. Styles for the search field.

--- glass_crm/main_forms/views.py
+++ glass_crm/main_forms/views.py
@@ -23,7 +23,7 @@ def create_entity(request, table_data, form_class, template_name, this_page):
     else:
         form = form_class
 
-    context = {'table_data': table_data, "form": form, "this_page": this_page}
+    context = {'table_data': table_data, "form": form, "this_page": this_page[0], "page_label": this_page[1]}
     return render(request, template_name, context=context)
 
 
@@ -32,24 +32,24 @@ def create_customer(request):
         num_contracts=Count('contracts'),     # Количество связанных договоров.
         total_amount=Sum('contracts__price')  # Общая сумма договоров.
     )
-    return create_entity(request, table_data, CustomersForm, "main_forms/customers.html", "customers")
+    return create_entity(request, table_data, CustomersForm, "main_forms/customers.html", ("customers", 'Клиенты'))
 
 
 def create_contract(request):
     subquery = Installations.objects.filter(contract=OuterRef('contract_number')).order_by('installation_date').values(
         'installation_date')[:1]
     table_data = Contracts.objects.annotate(installation_date=Subquery(subquery))
-    return create_entity(request, table_data, ContractsForm, "main_forms/contracts.html", "contracts")
+    return create_entity(request, table_data, ContractsForm, "main_forms/contracts.html", ("contracts", "Договора"))
 
 
 def create_order(request):
     table_data = Orders.objects.all()
-    return create_entity(request, table_data, OrdersForm, "main_forms/orders.html", "orders")
+    return create_entity(request, table_data, OrdersForm, "main_forms/orders.html", ("orders", "Заказы"))
 
 
 def create_metrics(request):
     table_data = Metrics.objects.all()
-    return create_entity(request, table_data, MetricsForm, "main_forms/metrics.html", "metrics")
+    return create_entity(request, table_data, MetricsForm, "main_forms/metrics.html", ("metrics", "Замеры"))
 
 
 def create_installation(request):
@@ -57,4 +57,4 @@ def create_installation(request):
         'square_meters')[:1]
     table_data = Installations.objects.annotate(square_meters=Subquery(subquery))
 
-    return create_entity(request, table_data, InstallationsForm, "main_forms/installations.html", "installations")
+    return create_entity(request, table_data, InstallationsForm, "main_forms/installations.html", ("installations", "Монтажи"))
