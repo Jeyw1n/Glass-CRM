bbdcbe9abba337b97382cc7a0698707068891499
bitweed <Oltibanta342@proton.me>
2024-01-07 18:11:34 +0300

    Добавление форм в админ панель

--- glass_crm/apps/main_forms/admin.py
+++ glass_crm/apps/main_forms/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.main_forms.models import Customers, Contracts, Orders, Metrics, Installations, Factories
+
+admin.site.register(Customers)
+admin.site.register(Contracts)
+admin.site.register(Orders)
+admin.site.register(Metrics)
+admin.site.register(Installations)
+admin.site.register(Factories)
