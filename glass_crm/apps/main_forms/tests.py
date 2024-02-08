a3cae645646cb09bfb3951ed7752a910884c61a4
bitweed <Oltibanta342@proton.me>
2024-02-08 14:52:51 +0300

    ост.

--- glass_crm/apps/main_forms/tests.py
+++ glass_crm/apps/main_forms/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Factories, Measurers
+from faker import Faker
 
-# Create your tests here.
+
+class MyTests(TestCase):
+    def setUp(self):
+        fake = Faker()
+
+        # Создаем 5 случайных заводов
+        for _ in range(5):
+            factory = fake.company()
+            Factories.objects.create(factory=factory)
+
+        # Создаем 5 случайных замерщиков
+        for _ in range(5):
+            measurer = fake.name()
+            Measurers.objects.create(measurer=measurer)
+
+    def test_factories_count(self):
+        factories_count = Factories.objects.count()
+        self.assertEqual(factories_count, 5)
+
+    def test_measurers_count(self):
+        measurers_count = Measurers.objects.count()
+        self.assertEqual(measurers_count, 5)
