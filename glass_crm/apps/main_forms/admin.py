56ab670079981c396bc1841b854d71a656a6fbc6
bitweed <Oltibanta342@proton.me>
2024-02-08 14:18:49 +0300

    Таблица замерщиков добавлена в админ панель

--- glass_crm/apps/main_forms/admin.py
+++ glass_crm/apps/main_forms/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Customers, Contracts, Orders, Metrics, Installations, Factories
+from .models import Customers, Contracts, Orders, Metrics, Installations, Factories, Measurers
 
 admin.site.register(Customers)
 admin.site.register(Contracts)
@@ -8,3 +8,4 @@ admin.site.register(Orders)
 admin.site.register(Metrics)
 admin.site.register(Installations)
 admin.site.register(Factories)
+admin.site.register(Measurers)
